2e3683401a2933b13ac69d2bdb7d369ff6f07bb1
yuktiPatel <77626936+yuktiPatel@users.noreply.github.com>
2021-04-30 10:48:10 +0530

    added comments

--- inc/activity1.h
+++ inc/activity1.h
@@ -15,10 +15,26 @@
 #define HEATER_BUTTON PD1
 
 //#define SEAT_OCCUPIED_HEATER_ON ((!((PIND) & (1 << SEAT))) == 1 && (!((PIND) & (1 << HEATER_BUTTON))) == 1) 
+/**
+ * @brief delay funtion
+ * 
+ */
 
 #define delay _delay_ms(1000)
 
+/**
+ * @brief pheripherals initialization
+ * 
+ */
+
 void peripherals_init(void);
+
+/**
+ * @brief funtion to check if seat is occupied and heater button is pressed
+ * 
+ * @return uint8_t 1-if seat occupied and heater button is pressed and 0-else
+ */
+
 uint8_t seatOccupiedAndHeaterOn(void);
 
 #endif
